d95871f4c6090926ec20f6fc71873ae247a87643
Vladimirrss <107629075+Vladimirrss@users.noreply.github.com>
2022-07-25 14:22:53 +0300

    Add files via upload

---
+++ Задание js/1 задание.js
@@ -0,0 +1,10 @@
+const userKazakov = {
+    Имя: 'Владими',
+    Фамилия: 'Казаков',
+    Возраст: '25'
+   }
+  const key1 = 'Имя';
+  const key2 = 'Фамилия';
+  const key3 = 'Возраст';
+  console.log(`${key1}: ${userKazakov[key1]}, ${key2}: ${userKazakov[key2]}, ${key3}: ${userKazakov[key3]}`)
+  
